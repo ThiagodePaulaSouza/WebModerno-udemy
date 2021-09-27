734eb4d74b7d8f652424cdd5d48875b01cebb45e
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2021-09-26 22:18:15 -0300

    minha solução

---
+++ Desafios/desafio3.js
@@ -0,0 +1,12 @@
+/*
+    fazer valor da variavel a ir pra variavel b
+    antes da troca a=7, b=94
+    dps da troca a=94, b=7
+*/
+let a = 7
+let b = 94
+console.log("var a = " + a + " var b = " + b)
+
+a = 94
+b = 7
+console.log("var a = " + a + " var b = " + b)
