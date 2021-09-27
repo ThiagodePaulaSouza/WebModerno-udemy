645c8e4252196e1a1ab509eefde7bc8d461d615b
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2021-09-26 22:06:01 -0300

    minha solução

---
+++ Desafios/desafio2.js
@@ -0,0 +1,11 @@
+/*
+    Calcular raio da conferencia
+    PI * raio * raio
+
+    constante é letra maiúscula
+*/
+const PI = 3.14
+const RAIO = 10
+const resultado = 3.14 * RAIO * RAIO
+
+console.log(resultado)
