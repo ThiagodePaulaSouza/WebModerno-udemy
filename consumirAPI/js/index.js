746c5e7336f7e8f7e282d8cee614f937ef288891
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2022-03-25 00:23:01 -0300

    v1

--- consumirAPI/js/index.js
+++ consumirAPI/js/index.js
@@ -1,1 +1,7 @@
-console.log("hello world")//funciona!
+'use strict'
+const consumirApi = () => {
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    fetch(url).then(response => response.json()).then(console.log);
+}
+document.getElementById('cep').addEventListener('focusout', consumirApi);
