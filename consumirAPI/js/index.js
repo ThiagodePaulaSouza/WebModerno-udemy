a77020ac679970e8cce189df9596c5c553c003c3
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2022-03-25 01:14:41 -0300

    tratamento de erro 🤓

--- consumirAPI/js/index.js
+++ consumirAPI/js/index.js
@@ -1,4 +1,12 @@
 'use strict'
+
+
+const limparFormulario = () => {
+    document.getElementById('endereco').value = "";
+    document.getElementById('bairro').value = "";
+    document.getElementById('cidade').value = "";
+    document.getElementById('estado').value = "";
+}
 const preemcherFormulario = (endereco) => {
     document.getElementById('endereco').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
@@ -6,12 +14,27 @@ const preemcherFormulario = (endereco) => {
     document.getElementById('estado').value = endereco.uf;
 }
 
+//expressão regular de lista 0 a 9, inicia-se com 0 e finaliza com 9
+const verificaNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const validarCep = (cep) => cep.length == 8 && verificaNumero(cep);
+
 const consumirApi = async() => {
+    limparFormulario();
     const cep = document.getElementById('cep').value;
     const url = `https://viacep.com.br/ws/${cep}/json/`;
-    const dados = await fetch(url);
-    const endereco = await dados.json();
-    preemcherFormulario(endereco);
-    // console.log(endereco);
+    if (validarCep(cep)) {
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if (endereco.erro) {
+            document.getElementById('endereco').value = "CEP não encontrado!";
+            console.log(endereco);
+        } else {
+            preemcherFormulario(endereco);
+        }
+    } else {
+        document.getElementById('endereco').value = "CEP incorreto!"
+        console.log(validarCep())
+    }
 }
 document.getElementById('cep').addEventListener('focusout', consumirApi);
