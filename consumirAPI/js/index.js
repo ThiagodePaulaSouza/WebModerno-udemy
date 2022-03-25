614d410075ea68f0af46fbd0cf710baa3fb0b45d
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2022-03-25 00:34:45 -0300

    api consumida 😋

--- consumirAPI/js/index.js
+++ consumirAPI/js/index.js
@@ -1,7 +1,17 @@
 'use strict'
-const consumirApi = () => {
+const preemcherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+const consumirApi = async() => {
     const cep = document.getElementById('cep').value;
     const url = `https://viacep.com.br/ws/${cep}/json/`;
-    fetch(url).then(response => response.json()).then(console.log);
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    preemcherFormulario(endereco);
+    // console.log(endereco);
 }
 document.getElementById('cep').addEventListener('focusout', consumirApi);
