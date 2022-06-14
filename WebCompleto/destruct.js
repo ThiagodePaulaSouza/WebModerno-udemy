c60cad03ffc6c0376c1c59783161f6e278d6bc02
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-06-14 18:07:54 -0300

    com array

--- WebCompleto/destruct.js
+++ WebCompleto/destruct.js
@@ -1,18 +1,24 @@
-const pessoa = {
-  nome: "thigas",
-  idade: 20,
-  endereco: {
-    logradouro: "Rua J",
-    numero: 10,
-  },
-};
+const [a] = [10]
+console.log(a)
 
-const { nome, idade } = pessoa;
-console.log(nome, idade);
 
-const { nome: n, idade: i } = pessoa;
-console.log(n, i);
+const [,[, nota]] = [["sangue", "morcego", 12], ["vampiro", 1234321, "suga"]] //confused
+console.log(nota)
+// const pessoa = {
+//   nome: "thigas",
+//   idade: 20,
+//   endereco: {
+//     logradouro: "Rua J",
+//     numero: 10,
+//   },
+// };
 
-const { endereco: { logradouro, numero }} = pessoa;
-console.log(logradouro);
-console.log(numero);
+// const { nome, idade } = pessoa;
+// console.log(nome, idade);
+
+// const { nome: n, idade: i } = pessoa;
+// console.log(n, i);
+
+// const { endereco: { logradouro, numero }} = pessoa;
+// console.log(logradouro);
+// console.log(numero);
