6358953326d32e05c8f80721f5a6b3fa62515dd0
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-06-14 18:26:10 -0300

    parametro chaves

--- WebCompleto/destruct.js
+++ WebCompleto/destruct.js
@@ -1,9 +1,19 @@
-const [a] = [10]
-console.log(a)
+function rand({ min = 0, max = 100 }) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+console.log(rand( {  } ))
+
+console.log(rand({ min: 50 }))
+console.log(rand({ max:1000 }))
+
+console.log(rand( { min: 0, max: 50 } ))
+
+// const [a] = [10]
+// console.log(a)
 
 
-const [,[, nota]] = [["sangue", "morcego", 12], ["vampiro", 1234321, "suga"]] //confused
-console.log(nota)
+// const [,[, nota]] = [["sangue", "morcego", 12], ["vampiro", 1234321, "suga"]] //confused
+// console.log(nota)
 // const pessoa = {
 //   nome: "thigas",
 //   idade: 20,
