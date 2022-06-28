a16d921fd36f4da32d03e6f90bb84bc3bb1a855f
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-06-28 19:44:35 -0300

    includes

--- Discover/index.js
+++ Discover/index.js
@@ -1,15 +1,12 @@
 const numeroQuebrado = 18926.1409282;
-const word = "aprender é legal porem não é facil"
+const word = "aprender é legal porem não é facil";
 
 console.log(
   `
-    trocar o numero de casas decimais:
-    ${numeroQuebrado.toFixed(3)}
-
-    trocar "." para ",":
-    ${numeroQuebrado.toFixed(5).replace(".", ",")}
-    
-    
+    verificar se tem uma palavra dentro de uma string:
+    texto: ${word}
+    chato: ${word.includes("chato")}
+    legal: ${word.includes("legal")}
 
   `
 );
