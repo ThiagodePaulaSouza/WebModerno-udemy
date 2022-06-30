7e93e3b74e557f066b21152b44421c783a8ba16c
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-06-29 22:17:31 -0300

    forof forin

--- Discover/index.js
+++ Discover/index.js
@@ -1,12 +1,21 @@
-function sayMyName(name = '') {
-    if (name === '') {
-        throw 'Nome é obrigatório'
-    }
-    console.log("bom dia")
+const name = 'Thigas'
+const names = ['marcelo', 'jão', 'vitoria']
+
+for (const char of name) {
+    console.log(char)
 }
 
-try {
-    sayMyName()
-} catch (e) {
-    console.log(e)
+for (const char of names) {
+    console.log(char)
+}
+
+const person = {
+    name: 'Mario',
+    age: 20,
+    weight: 55.9,
+}
+
+for (const sla in person) {
+    console.log(sla)
+    console.log(person[sla])
 }
