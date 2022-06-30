30183d4011204f3310d66ae39b9a2f6c15ac55fd
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-06-29 22:10:44 -0300

    try catch throw

--- Discover/index.js
+++ Discover/index.js
@@ -1,29 +1,12 @@
-const teste = ["python", "javascript", "csharp", "java", "lua"];
+function sayMyName(name = '') {
+    if (name === '') {
+        throw 'Nome é obrigatório'
+    }
+    console.log("bom dia")
+}
 
-// "adicionar um item no final"
-teste.push("php");
-console.log(teste);
-
-// "adicionar no começo"
-teste.unshift("sql");
-console.log(teste);
-
-// "remover do fim"
-teste.pop();
-console.log(teste);
-
-// "remover do começo"
-teste.shift();
-console.log(teste);
-
-// pegar somente alguns elementos em qualquer posição do array
-console.log(teste.slice(2, 5));
-
-// "remover 1 ou mais items em qualquer posição do array"
-teste.splice(1, 1);
-console.log(teste);
-
-// encontrar a posição de um elemento no array
-let index = teste.indexOf("lua");
-teste.splice(index, 1);
-console.log(teste);
+try {
+    sayMyName()
+} catch (e) {
+    console.log(e)
+}
