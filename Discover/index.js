3bc893dd15e5f8008fbd69ef92a3122b2b25ddd4
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-02 22:55:18 -0300

    transcrevendo o exercicio

--- Discover/index.js
+++ Discover/index.js
@@ -1,1 +1,13 @@
-//Sistema de notas escolares
+/*
+ *  # Transformar notas escolares
+ *
+ *  crie um algoritmo que tranforme as notas do sistema
+ *  numerico para sistema de notas em caracteres tipo ABC
+ *
+ *  90 >    -> A
+ *  80 - 89 -> B
+ *  70 - 79 -> C
+ *  60 - 69 -> D
+ *  < 60    -> F
+ *
+ * */
