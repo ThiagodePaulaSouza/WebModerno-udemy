c6409baed18a15653baf8eaf841ae68a482d7938
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-05 22:50:39 -0300

    eventos

--- Discover/script.js
+++ Discover/script.js
@@ -1,24 +1,29 @@
-// Criando e adicionando elementos
+const h1 = document.querySelector('h1')
 
-// createElement
-const div = document.createElement('div')
-div.innerText = "Olá div"
-
-const div2 = document.createElement('div')
-div2.innerText = "Olá div2"
-
-const div3 = document.createElement('div')
-div3.innerText = "Olá div3"
-
-// append prepend
-const body = document.querySelector('body')
-body.append(div)
-body.prepend(div2)
+h1.addEventListener('click', print)
 
 
-// insert before
-const script = body.querySelector('script')
-body.insertBefore(div, script)
+function print() {
+    console.log("Print");
+}
 
-const header = body.querySelector('header')
-body.insertBefore(div3, header.nextElementSibling)
+const input = document.querySelector('input')
+
+// input.onkeydown = function () {
+//     console.log('apertei')
+// }
+
+// input.onkeyup = function () {
+//     console.log('soltei')
+// }
+
+// input.onkeypress = function () {
+//     console.log('digitei')
+// }
+
+input.onkeydown = (event) => {
+    console.log(event)
+    console.log(event.key)
+    console.log(event.currentTarget)
+    console.log(event.currentTarget.value)
+}
