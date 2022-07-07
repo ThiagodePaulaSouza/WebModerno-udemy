2265944e2f5b3e08a6bbe6e49bbad449186bfd2c
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-07 01:45:31 -0300

    NodeEJS: finish

--- Discover/nodeJsEJS/server.js
+++ Discover/nodeJsEJS/server.js
@@ -4,7 +4,38 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("pages/index");
+  const items = [
+    {
+      title: "D",
+      message: "esenvolver é legal",
+    },
+    {
+      title: "E",
+      message: "JS usa js pra renderizar HTML",
+    },
+    {
+      title: "M",
+      message: "uito simples de usar",
+    },
+    {
+      title: "A",
+      message: "mado por alguns odiado por outros",
+    },
+    {
+      title: "I",
+      message: "nstall ejs",
+    },
+    {
+      title: "S",
+      message: "intaxe diferente",
+    },
+  ];
+
+  const subtitle = "Uma linguagem de modelagem para criação de pagina HTML"
+  res.render("pages/index", {
+    qualitys: items,
+    subtitle: subtitle,
+  });
 });
 
 app.get("/sobre", function (req, res) {
