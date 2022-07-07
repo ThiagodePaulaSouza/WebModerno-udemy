336dbd8eeda08f151b537f55573273375532e934
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-06 21:55:15 -0300

    node: first commit
    variaveis globais, require

---
+++ Discover/nodeJs/console.js
@@ -0,0 +1,12 @@
+// variaveis globais do node
+console.log(global);
+
+diretório
+console.log(__dirname);
+console.log(__filename);
+
+require
+
+const path = require('path')
+
+console.log(path.basename(__filename))
