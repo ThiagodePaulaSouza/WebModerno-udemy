60417199face56ea823bade5c5037646748d051e
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-08 19:58:16 -0300

    conceito de promisse

---
+++ Discover/Especializar/promisse.js
@@ -0,0 +1,5 @@
+// promisse tem 4 estágios
+Pending -> estado inicial, assim que o objeto da promessa é iniciado
+Fulfilled -> a promessa foi concluída com sucesso
+Rejected -> a promessa foi rejeitada, houve um erro
+settled -> seja com sucesso ou com erro, ela foi finalmente concluida
