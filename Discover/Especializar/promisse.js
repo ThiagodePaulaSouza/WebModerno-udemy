a7abe5ebb1c49b956f790cbd30b188edcb5cf097
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-08 20:16:53 -0300

    promisse resolve, reject then, catch, finally

--- Discover/Especializar/promisse.js
+++ Discover/Especializar/promisse.js
@@ -1,5 +1,23 @@
 // promisse tem 4 estágios
-Pending -> estado inicial, assim que o objeto da promessa é iniciado
-Fulfilled -> a promessa foi concluída com sucesso
-Rejected -> a promessa foi rejeitada, houve um erro
-settled -> seja com sucesso ou com erro, ela foi finalmente concluida
+// Pending -> estado inicial, assim que o objeto da promessa é iniciado
+// Fulfilled -> a promessa foi concluída com sucesso
+// Rejected -> a promessa foi rejeitada, houve um erro
+// settled -> seja com sucesso ou com erro, ela foi finalmente concluida
+
+const aceitar = true;
+
+console.log("Pedindo lanche");
+
+const promessa = new Promise((resolve, reject) => {
+  if (aceitar) {
+    return resolve("deu certo!");
+  }
+  return reject("não deu certo 😴");
+});
+
+console.log("aguardando");
+
+promessa
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err))
+  .finally(() => console.log("finalizado"));
