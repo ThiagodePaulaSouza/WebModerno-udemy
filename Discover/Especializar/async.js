05c8a42ad8467c7f1a9db4df62bd67230be3ea63
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-08 21:17:15 -0300

    com async

--- Discover/Especializar/async.js
+++ Discover/Especializar/async.js
@@ -1,5 +1,21 @@
 const promessa = new Promise( (resolve, reject) => resolve('ok') )
-promessa
-.then((response) => console.log(response))
-.catch((err) => console.log(err))
-.finally(() => console.log('sempre irei executar'))
+
+async function start() {
+    try{
+        const result = await promessa
+        console.log(result);
+    }
+    catch( e ){
+        console.log(e);
+    }
+    finally {
+        console.log("Rodar sempre");
+    }
+}
+
+start()
+
+// promessa
+// .then((response) => console.log(response))
+// .catch((err) => console.log(err))
+// .finally(() => console.log('sempre irei executar'))
