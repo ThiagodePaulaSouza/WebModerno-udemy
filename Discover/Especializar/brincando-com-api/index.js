aac895c41e0e34db81f53b2d119259bbdc52376b
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-09 03:02:09 -0300

    🛶API: api no frontend com axios

--- Discover/Especializar/brincando-com-api/index.js
+++ Discover/Especializar/brincando-com-api/index.js
@@ -1,15 +1,79 @@
 const express = require('express')
-const axios = require('axios')
+const cors = require('cors')
+
 const app = express()
 
-app.listen(3000)
+app.listen(5000, () => console.log('Rodando na porta 5000'))
 
+app.use(cors())
 
-// midware
 app.use(express.json())
 
-app.route('/').get((req, res) => {
-    axios.get('https://api.github.com/users/thiagodepaulasouza')
-    .then(result => res.send(`<img src="${result.data.avatar_url}" />`))
-    .catch(error => console.error(error))
+let users = [{
+  id: 1,
+  name: "Thiago de Paula Souza",
+  avatar: "https://avatars.githubusercontent.com/u/75866888?v=4",
+  city: "Sorocaba"
+}]
+
+
+app.route('/api').get((req, res) => res.json({
+  users
+}))
+
+app.route('/api/:id').get((req, res) => {
+  const userId = req.params.id
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  if (!user) {
+    return res.json('User nor found!')
+  }
+
+  res.json(user)
+})
+
+app.route('/api').post((req, res) => {
+  const lastId = users[users.length - 1].id
+  users.push({
+    id: lastId + 1,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    city: req.body.city
+  })
+  res.json('Saved user')
+})
+
+app.route('/api/:id').put((req, res) => {
+  const userId = req.params.id
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  if (!user) {
+    return res.json('User nor found!')
+  }
+
+  const updatedUser = {
+    ...user,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    city: req.body.city
+  }
+
+  users = users.map(user => {
+    if (Number(user.id) === Number(userId)) {
+      user = updatedUser
+    }
+    return user
+  })
+
+  res.json("Updated user")
+})
+
+app.route('/api/:id').delete((req, res) => {
+  const userId = req.params.id
+
+  users = users.filter(user => Number(user.id) !== Number(userId))
+
+  res.json('Deleted User')
 })
