31280ac53cf08ef2c1e12f3ea7e36faefbbece91
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-08 23:20:39 -0300

    🛶API: DELETE

--- Discover/Especializar/brincando-com-api/index.js
+++ Discover/Especializar/brincando-com-api/index.js
@@ -5,13 +5,8 @@ app.listen(3000)
 
 
 // midware
-app.use(express.json())
 
-let author = "thigas"
 
-app.route('/').get( (req, res) => res.send(author) )
-
-app.route('/').put( (req, res) => {
-    author = req.body.author
-    res.send(author)
-} )
+app.route('/:identificador').delete((req, res) => {
+    res.send(req.params.identificador)
+})
