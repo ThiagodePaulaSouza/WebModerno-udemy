3a85d10fcba2d9646b781498a5d91b6de03b4ae8
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-08 21:28:18 -0300

    async -> encadeado

--- Discover/Especializar/async.js
+++ Discover/Especializar/async.js
@@ -1,21 +1,28 @@
-const promessa = new Promise( (resolve, reject) => resolve('ok') )
+// sem async
+// fetch('https://api.github.com/users/thiagodepaulasouza')
+// .then( (response) => response.json())
+// .then( (data) => fetch(data.repos_url))
+// .then( res => res.json())
+// .then( d => console.log(d))
+// .catch( err => console.log(err))
 
+// com async
 async function start() {
-    try{
-        const result = await promessa
-        console.log(result);
-    }
-    catch( e ){
-        console.log(e);
-    }
-    finally {
-        console.log("Rodar sempre");
-    }
+    const response = await fetch('https://api.github.com/users/thiagodepaulasouza')
+    const user = await response.json()
+    const reposResponse = await fetch(user.repos_url)
+    const repos = await reposResponse.json()
+    console.log(repos);
 }
 
-start()
+start().catch(e => console.log(e))
 
-// promessa
-// .then((response) => console.log(response))
-// .catch((err) => console.log(err))
-// .finally(() => console.log('sempre irei executar'))
+// outra forma com async
+async function start() {
+    const url = 'https://api.github.com/users/thiagodepaulasouza'
+    const user = await fetch(url).then(u => u.json())
+    const userRepos = await fetch(user.repos_url).then(r => r.json())
+    console.log(userRepos);
+}
+
+start().catch(e => console.log(e))
