e14cca68b3f616ee00c2f4298051b763771ef863
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-08 23:25:49 -0300

    🛶API: passando parametros para o navegador

--- Discover/Especializar/brincando-com-api/index.js
+++ Discover/Especializar/brincando-com-api/index.js
@@ -5,8 +5,11 @@ app.listen(3000)
 
 
 // midware
+app.use(express.json())
 
+app.route('/').get( (req, res) => res.send(req.query.name) )
+app.route('/').put( (req, res) => res.send(req.body.author))
+app.route('/:parametro').get( (req, res) => res.send(req.params.parametro))
 
-app.route('/:identificador').delete((req, res) => {
-    res.send(req.params.identificador)
-})
+//http://localhost:3000/?name=thigas
+//aparece thigas no navegador
