712315f4df1cd41b596f1a8dfc2ec9774198edef
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-18 23:19:36 -0300

    array: concat

---
+++ WebCompleto/Array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ["roberta", "marcelina"];
+const filhos = ["roberto", "marcelo"];
+const todos = filhos.concat(filhas);
+
+console.log(filhas, filhos, todos);
+
+console.log([].concat([1,2], [123,435], 9, [[123129, "oguipsaf"]]));
