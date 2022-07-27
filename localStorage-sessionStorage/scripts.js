e0579f07e64a2551074c53f77a6cc133980c2329
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-27 16:41:16 -0300

    add: new project
    localStorage: 10mb, persistence
    
    sessionStorage: 5mb, not persistence

---
+++ localStorage-sessionStorage/scripts.js
@@ -0,0 +1,14 @@
+/**
+ * LocalStorage
+ * Recurso que salva dados no navegador que persiste após o recarregamento da página
+ + Capacidade maxima de 10mb
+ * n tem tempo de expiração mas podem ser removidos 
+ * os dados ficam na aba Application no chrome dev toots
+**/ 
+/**
+ * SessionStorage
+ * similar ao localStorage
+ + capacidade maxima de 5mb
+ * os dados exiram quando a aba é fechada
+ * os dados ficam na aba Application no chrome dev toots
+ */
