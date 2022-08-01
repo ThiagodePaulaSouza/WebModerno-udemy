00c63ba1754e7d7ef8d1ffd68ed0dbbac72f779d
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-31 23:51:44 -0300

    GET ok

--- ApiNodePostgree/index.js
+++ ApiNodePostgree/index.js
@@ -7,10 +7,54 @@ const cliente = new Client({
   database: 'bdmembro'
 })
 
+let result;
+
 cliente.connect()
 cliente.query('select * from tb_membro')
 .then(res => {
-    const resultado = res.rows 
-    console.log(resultado);
-  })
+    resultado = res.rows 
+    })
   .finally(() => cliente.end())
+
+
+  const express = require('express')
+  const cors = require('cors')
+  const app = express()
+  const port = 3000
+
+  app.use(cors())
+  app.use(express.json())
+
+  app.get('/', (req, res) => res.send('Hello World!'))
+
+  // HTTP: GET
+  app.route('/api').get((req, res) => res.json(resultado))
+
+  // HTTP: GET ID
+  app.route('/api/:id').get((req, res) => {
+    const userId = req.params.id
+  
+    const user = resultado.find(user => Number(user.id) === Number(userId))
+  
+    if (!user) {
+      return res.json('User nor found!')
+    }
+  
+    res.json(user)
+  })
+
+  // // HTTP: POST
+  // app.route('/api').post((req, res) => {
+  //   const lastId = resultado[resultado.length - 1].id
+  //   resultado.push({
+  //     id: lastId + 1,
+  //     nome: req.body.nome,
+  //     email: req.body.email,
+  //     github: req.body.github,
+  //     phone: req.body.phone
+  //   })
+  //   res.json('Saved user')
+  // })
+  // HTTP: DELETE
+  
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
