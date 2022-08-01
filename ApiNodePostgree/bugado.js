d9b56079496eb14880f8c9c32d73636809f4c4e3
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-31 23:28:08 -0300

    teste

---
+++ ApiNodePostgree/bugado.js
@@ -0,0 +1,16 @@
+const Client = require('pg').Client
+const cliente = new Client({
+  user: 'postgres',
+  password: 'thiago3213',
+  host: 'localhost',
+  port: '5432',
+  database: 'bdmembro'
+})
+
+cliente.connect()
+cliente.query('select * from bdmembros')
+.then(res => {
+    const resultado = res.rows 
+    console.log(resultado);
+  })
+  .finally(() => cliente.end())
