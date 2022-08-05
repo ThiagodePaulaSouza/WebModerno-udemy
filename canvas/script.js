4ead7caf174e96a395e5ac65480ae4245ee1641f
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-08-05 20:36:20 -0300

    desenhando na tela e selecionando a cor

--- canvas/script.js
+++ canvas/script.js
@@ -1,28 +1,36 @@
-var tela = document.querySelector("canvas");
-var pincel = tela.getContext("2d");
 
-pincel.fillStyle = "grey";
-pincel.fillRect(0, 0, 600, 400);
+    var paleta = document.querySelector('input');
+    var tela = document.querySelector('canvas');
+    var pincel = tela.getContext('2d');
 
-var desenha = false;
+    pincel.fillStyle = 'grey';
+    pincel.fillRect(0, 0, 600, 400);
 
-// atribuindo diretamente a função anônima
-tela.onmousemove = function (evento) {
-  if (desenha) {
-    var x = evento.pageX - tela.offsetLeft;
-    var y = evento.pageY - tela.offsetTop;
-    pincel.fillStyle = "blue";
-    pincel.beginPath();
-    pincel.arc(x, y, 5, 0, 2 * 3.14);
-    pincel.fill();
-  }
-  console.log(x + "," + y);
-};
+    var desenha = false;
 
-tela.onmousedown = function () {
-  desenha = true;
-};
+    function desenhaCirculo(evento) {
 
-tela.onmouseup = function () {
-  desenha = false;
-};
+        if(desenha) {
+            var x = evento.pageX - tela.offsetLeft;
+            var y = evento.pageY - tela.offsetTop;
+            pincel.fillStyle = paleta.value; // sempre pega o valor atual da paleta!
+            pincel.beginPath();
+            pincel.arc(x, y, 10, 0, 2 * 3.14);
+            pincel.fill();
+        }
+        console.log(x + ',' + y);
+    }
+
+    tela.onmousemove = desenhaCirculo;
+
+    // atribuindo diretamente a função anônima
+    tela.onmousedown = function() {
+
+        desenha = true;
+    }
+
+    // atribuindo diretamente a função anônima
+     tela.onmouseup = function() {
+
+        desenha = false;
+    }
