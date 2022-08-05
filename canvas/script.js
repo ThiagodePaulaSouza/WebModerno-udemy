f943dfb24316a419589105f59e72d077c33a410b
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-08-05 18:44:50 -0300

    badeira feia

--- canvas/script.js
+++ canvas/script.js
@@ -1,4 +1,29 @@
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
+const cc = canvas.getContext("2d");
+cc.fillStyle = "lightgrey";
+cc.fillRect(0, 0, 600, 350);
+
+cc.fillStyle = 'blue';
+cc.fillRect(0, 0, 200, 350);
+
+cc.fillStyle = 'red';
+cc.fillRect(400, 0, 200, 350);
+
+cc.fillStyle = 'green'
+cc.beginPath();
+cc.moveTo(300, 175)
+cc.lineTo(200, 350);
+cc.lineTo(400, 350);
+cc.fill()
+
+cc.fillStyle = 'green'
+cc.beginPath();
+cc.moveTo(300, 175)
+cc.lineTo(200, 0);
+cc.lineTo(400, 0);
+cc.fill()
+
+cc.fillStyle = 'green'
+cc.beginPath()
+cc.arc(300, 175, 50, 0, 2*3.14)
+cc.fill()
